db45e89a99ab14b342f69300a486a669c515d963
Guilherme Marques <g-marques@outlook.com>
2023-08-24 01:36:54 -0300

    refactor: Define styles for texts

--- App.tsx
+++ App.tsx
@@ -2,9 +2,29 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>React</Text>
-      <Text>Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48,
+      }}>
+        Nome do Evento
+      </Text>
+
+      <Text
+        style={{
+          color: '#6b6b6b',
+          fontSize: 16,
+        }}
+      >
+        Sexta-feira, 25 de agosto de 2023
+      </Text>
     </View>
   );
 }
